efd4897d2b0e6c81bd3eb48366af25a2a67fc3d0
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2022-02-12 21:31:45 +0200

    modal

--- src/pixabay.js
+++ src/pixabay.js
@@ -2,10 +2,10 @@ import axios from "axios";
 axios.defaults.baseURL = "https://pixabay.com/api/";
 const KEY = "24426317-0934d3933860248e2f67d0ecb";
 
-async function fetchPictures(name, page = 1) {
+async function fetchPictures(query, page = 1) {
   try {
     const response = await axios.get(
-      `?q=${name}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      `?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
     );
     return response.data;
   } catch (error) {
@@ -18,3 +18,4 @@ const ImagesAPI = {
   fetchPictures,
 };
 export default ImagesAPI;
+
